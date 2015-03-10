3f0ac134c3c26d6d984c51068003b2ad719762f6
omrilitov <omrilitov@gmail.com>
2015-03-11 01:50:53 +0200

    Basic configuration and dips

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ gulp.task('default', ['test']);
 gulp.task('test', ['jshint']);
 
 gulp.task('jshint', function () {
-  gulp.src('./lib/*.js')
+  gulp.src('./lib/**/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter(stylish))
     .pipe(jshint.reporter('fail'));
